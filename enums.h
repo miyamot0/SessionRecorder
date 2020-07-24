56ab423114ca64260892f48593b2e41711c17cb5
Shawn Gilroy <shawn.gilroy@temple.edu>
2020-07-23 19:19:33 -0500

    Licensing stuff

--- enums.h
+++ enums.h
@@ -1,3 +1,30 @@
+/****************************************************************************
+
+    Copyright 2018 Shawn Gilroy
+
+    This file is part of Session Recorder.
+
+    Session Recorder is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    Session Recorder is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Session Recorder.  If not, see http://www.gnu.org/licenses/.
+
+    The Session Recorder is a tool to assist researchers in clinical behavioral research.
+
+    This file was adapted from meeting-recorder (MIT), which was based on Qt Examples
+    provided by Digia (BSD-3)
+
+    Email: shawn(dot)gilroy(at)temple.edu
+
+****************************************************************************/
+
 #ifndef ENUMS_H
 #define ENUMS_H
 
--- recordsettings.h
+++ recordsettings.h
@@ -1,3 +1,30 @@
+/****************************************************************************
+
+    Copyright 2018 Shawn Gilroy
+
+    This file is part of Session Recorder.
+
+    Session Recorder is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    Session Recorder is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Session Recorder.  If not, see http://www.gnu.org/licenses/.
+
+    The Session Recorder is a tool to assist researchers in clinical behavioral research.
+
+    This file was adapted from meeting-recorder (MIT), which was based on Qt Examples
+    provided by Digia (BSD-3)
+
+    Email: shawn(dot)gilroy(at)temple.edu
+
+****************************************************************************/
+
 #ifndef RECORDSETTINGS_H
 #define RECORDSETTINGS_H
 
@@ -21,30 +48,30 @@ class RecordSettings
 {
     public:
 
-        RecordSettings::RecordSettings()
+        RecordSettings()
         {
             data = new RecordSettingsData;
         }
 
-        RecordSettings::RecordSettings(const RecordSettings& other)
+        RecordSettings(const RecordSettings& other)
         {
             data = new RecordSettingsData(*other.data);
         }
 
-        RecordSettings::~RecordSettings()
+        ~RecordSettings()
         {
             delete data;
         }
 
-        RecordSettings& RecordSettings::operator=( const RecordSettings& other )
+        RecordSettings& operator=( const RecordSettings& other )
         {
             *data = *other.data;
             return *this;
         }
 
-        void RecordSettings::storeData(const QString &ffmpegLocation, const QString &fileSaveLocation,
-                                       const QString &mVideoDevice, const QString &mVideoFPS, const QString &mResolution,
-                                       const QString &mAudioDevice, const QString &mAudioEncoding, const QString &mAudioSampling)
+        void storeData(const QString &ffmpegLocation, const QString &fileSaveLocation,
+                       const QString &mVideoDevice, const QString &mVideoFPS, const QString &mResolution,
+                       const QString &mAudioDevice, const QString &mAudioEncoding, const QString &mAudioSampling)
         {
             data->ffmpegLocation   = ffmpegLocation;
             data->fileSaveLocation = fileSaveLocation;
@@ -58,7 +85,7 @@ class RecordSettings
             data->mAudioSampling   = mAudioSampling;
         }
 
-        RecordSettingsData* RecordSettings::getData()
+        RecordSettingsData* getData()
         {
             return data;
         }
